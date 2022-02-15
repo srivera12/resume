3e9397c786ccf7d586dddcf18c574baee918e5b4
srivera12 <sarahkrivera@gmail.com>
2022-02-15 14:47:39 -0500

    deployment asset fix

---
+++ _next/static/5AuOdZAqEO7G6SMogT6Ly/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcourses\u002F[courseName]","\u002Fproject-pages\u002F[projectName]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/EMQ2PaYqzIpv46t1WIW-K/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fproject-pages\u002F[projectName]","\u002Fcourses\u002F[courseName]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
